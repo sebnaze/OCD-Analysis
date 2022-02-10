51a722e487433d706b7302009f41c8c4c5ae051a
sNaze <sebastien.naze@gmail.com>
2022-02-10 20:35:26 +1000

    update sc_fc analysis with NAcc-OFC dPut-lPFC FC and SC ++ streamlined the code so any atlas, rois, subrois SC-FC abalytsis can be ran (only tested for the latest, handmade ocd specific atlas)

--- scfc_analysis.py
+++ scfc_analysis.py
@@ -43,10 +43,6 @@ with open(atlas_cfg_path) as jsf:
     atlas_cfg = json.load(jsf)
 subjs = pd.read_table(os.path.join(code_dir, 'subject_list.txt'), names=['name'])['name']
 
-# options
-atlases = ['schaefer100_tianS1', 'schaefer200_tianS2', 'schaefer400_tianS4']
-fc_metrics = ['detrend_gsr_filtered', 'detrend_filtered']
-sc_metrics = ['count_sift', 'count_nosift']
 cohorts = ['controls', 'patients']
 
 # rois of the FrStrThal Atlases to exclude from analysis
@@ -54,6 +50,19 @@ excl_rois = { 'fspt':[], \
               'Fr':['Thal', 'Pal', 'Put', 'Caud', 'Acc'], \
               'StrTh':['PFC', 'OFC', 'Fr', 'FEF', 'ACC', 'Cing', 'PrC', 'ParMed'] }
 
+# set here which edges to analyze based on the atlas
+atlas_types = {'ocdOFClPFC_ocdAccdPut': { 'atlases' : ['ocdOFClPFC_ocdAccdPut'],
+                                          'subrois' : [['AccSPM_R'], ['PutSPM_R']],
+                                          'rois' : {'AccSPM_R': ['OFC'], 'PutSPM_R':['PFC']},
+                                          'file_suffix' : {'SC': '_ocd', 'FC':'_ocd'} },#{'SC': '_harrison2009', 'FC':'_harrison2009_fwhm8'} },
+               'schaefer400_tianS4':    { 'atlases' : ['schaefer400_tianS4'],
+                                          'subrois' : None,
+                                          'rois' : None, # None will compute SC-FC for all edges!
+                                          'file_suffix' : {'SC':'', 'FC':''} },
+               'schaefer400_harrison2009': { 'atlases' : ['schaefer400_harrison2009'],
+                                             'subrois' : [['Acc'], ['dPut']],
+                                             'rois' : {'Acc': ['Right_ContB_PFClv'], 'dPut':['Right_ContA_PFCl']},
+                                             'file_suffix' : {'SC':'_ocd', 'FC':'_ocd'} } }
 
 # plotting utils
 plt_utl = {'controls':'blue',
@@ -181,19 +190,19 @@ def plot_scfc_distrib(corrs, scm='count_nosift', fcm='detrend_filtered', bins=10
         plt.ylabel('n_subjs')
     plt.show(block=False)
 
-def plot_scfc_pvals(outp_scfc, subnet, atlases, sc_metrics, fc_metrics):
+def plot_scfc_pvals(outp_scfc, subnet, subroi, atlases, sc_metrics, fc_metrics):
     """ plot bar plots of p-values """
     plt.figure(figsize=[16,4])
     for i,atlas in enumerate(atlases):
         plt.subplot(1,3,i+1)
         cnt=1
         for scm,fcm in itertools.product(sc_metrics, fc_metrics):
-            plt.bar(cnt, outp_scfc[subnet,atlas,scm,fcm]['p'], label='{}-{}'.format(scm.split('_')[1], fcm.split('_')[1][:3]), alpha=0.7)
+            plt.bar(cnt, outp_scfc[subnet,subroi,atlas,scm,fcm]['p'], label='{}-{}'.format(scm.split('_')[1], fcm.split('_')[1][:3]), alpha=0.7)
             cnt+=1
         plt.ylim([0,1])
         plt.axhline(y=0.05, linestyle='--', c='red')
         plt.legend()
-        plt.title('{} - {}'.format(subnet,atlas))
+        plt.title('{} - {} - {}'.format(subnet,subroi,atlas))
     plt.show(block=False)
 
 
@@ -234,13 +243,22 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_figs', default=False, action='store_true', help='save figures')
     parser.add_argument('--save_outputs', default=False, action='store_true', help='save outputs')
+    parser.add_argument('--output_suffix', type=str, default='_ocd', action='store', help='output suffix for saving correlations')
     parser.add_argument('--sc_threshold', type=float, default=None, action='store', help='threshold applied to structutal connectome [0-1]')
+    parser.add_argument('--atlas_type', type=str, default='ocdOFClPFC_ocdAccdPut', action='store', help='VOI atlas, can be ocdOFClPFC_ocdAccdPut (default, i.e. Acc-OFC dPut-PFC), schaefer400_harrison2009, or schaefer400_tianS4')
     args = parser.parse_args()
 
+    # options
+    atlases = ['ocdOFClPFC_ocdAccdPut'] #['schaefer100_tianS1', 'schaefer200_tianS2', 'schaefer400_tianS4']
+    fc_metrics = ['detrend_gsr_filtered', 'detrend_filtered']
+    sc_metrics = ['count_sift'] #['count_sift', 'count_nosift']
+
     # load data
-    with open(os.path.join(proj_dir, 'postprocessing', 'conns_SC.pkl'), 'rb') as scf:
+    with open(os.path.join(proj_dir, 'postprocessing',
+                            'conns_SC'+atlas_types[args.atlas_type]['file_suffix']['SC']+'.pkl'), 'rb') as scf:
         conns_sc = pickle.load(scf)
-    with open(os.path.join(proj_dir, 'postprocessing', 'conns_FC.pkl'), 'rb') as fcf:
+    with open(os.path.join(proj_dir, 'postprocessing',
+                            'conns_FC'+atlas_types[args.atlas_type]['file_suffix']['FC']+'.pkl'), 'rb') as fcf:
         conns_fc = pickle.load(fcf)
 
     # remove patients 28 & 55 from FC since not in SC
@@ -250,35 +268,66 @@ if __name__=='__main__':
         if k[2]=='patients':
             conns_fc[k] = np.delete(conn[:,:,:-1], pt28_idx, axis=-1)
 
+    # create summary dataframe
+    scfc_summary_df = pd.DataFrame()
+    for i,subj in enumerate(subjs):
+        if ( ('patient28' in subj) or ('patient55' in subj) ):
+            subjs.drop(i)
+    #scfc_summary_df['subj'] = subjs
+
+
     # extract correlation and MI for each sub-network
     subnets = ['wholebrain', 'fspt', 'Fr', 'StrTh']
+    subrois = atlas_types[args.atlas_type]['subrois']
     subnet_corrs = dict()
-    for subnet in subnets:
-        subnet_corrs[subnet] = scfc_corr(subnet, atlases, sc_metrics, fc_metrics, conns_sc, conns_fc, sc_threshold=args.sc_threshold)
+    for subnet, subroi in itertools.product(subnets,subrois):
+        row_rois = np.array(subroi).flatten()
+        col_rois = np.array([atlas_types[args.atlas_type]['rois'][roi] for roi in row_rois]).flatten()
+        sbr = '_'.join(subroi)
+        subnet_corrs[subnet,sbr] = scfc_corr(subnet, atlases, sc_metrics, fc_metrics, conns_sc, conns_fc,
+                                         row_rois=row_rois, col_rois=col_rois, sc_threshold=args.sc_threshold)
+
+    # save in summary dataframe
+    for subnet,subroi,atlas,scm,fcm in itertools.product(subnets,subrois,atlases,sc_metrics,fc_metrics):
+        if ((subnet_corrs[subnet,sbr][atlas,scm,fcm,'controls'] != None) and (subnet_corrs[subnet,sbr][atlas,scm,fcm,'patients'] != None)):
+            sbr = '_'.join(subroi)
+            df_ = pd.DataFrame(subjs)
+            df_['subnet'] = np.repeat(subnet, len(subjs))
+            df_['subroi'] = np.repeat(sbr, len(subjs))
+            df_['atlas'] = np.repeat(atlas, len(subjs))
+            df_['sc_metric'] = np.repeat(scm, len(subjs))
+            df_['fc_metric'] = np.repeat(fcm, len(subjs))
+            df_['sc'] = np.repeat(np.NaN, len(subjs))
+            df_['fc'] = np.repeat(np.NaN, len(subjs))
+            inds = np.concatenate([subnet_corrs[subnet,sbr][atlas,scm,fcm,coh]['subjs_inds'] for coh in cohorts])
+            df_['sc'].iloc[inds] = np.concatenate([subnet_corrs[subnet,sbr][atlas,scm,fcm,coh]['scs'] for coh in cohorts])
+            df_['fc'].iloc[inds] = np.concatenate([subnet_corrs[subnet,sbr][atlas,scm,fcm,coh]['fcs'] for coh in cohorts])
+            scfc_summary_df = scfc_summary_df.append(df_, ignore_index=True)
 
     # save SC-FC correlations and MI
     if args.save_outputs:
-        with open(os.path.join(proj_dir, 'postprocessing', 'corrs_SCFC.pkl'), 'wb') as pf:
+        with open(os.path.join(proj_dir, 'postprocessing', 'corrs_SCFC'+args.output_suffix+'.pkl'), 'wb') as pf:
             pickle.dump(subnet_corrs, pf)
 
 
-
     # extract stats
-    outp_scfc = dict( ((subnet, atlas,scm,fcm),None) for subnet,atlas,scm,fcm in itertools.product(subnets, atlases, sc_metrics, fc_metrics) )
+    outp_scfc = dict( ( (subnet,'_'.join(subroi),atlas,scm,fcm),None) for subnet,subroi,atlas,scm,fcm in itertools.product(subnets, subrois, atlases, sc_metrics, fc_metrics) )
     p_min = [1., [None,None,None,None]] # [p, key]
     excl_subnets = []
-    for subnet,atlas,scm,fcm in itertools.product(subnets, atlases, sc_metrics, fc_metrics):
-        if ( (subnet_corrs[subnet][atlas,scm,fcm,'controls'] != None) and (subnet_corrs[subnet][atlas,scm,fcm,'patients'] != None) ):
-            t,p = scipy.stats.ttest_ind(subnet_corrs[subnet][atlas,scm,fcm,'controls']['r'], subnet_corrs[subnet][atlas,scm,fcm,'patients']['r'], permutations=1000)
-            outp_scfc[subnet,atlas,scm,fcm] = {'t':t, 'p':p}
+    for subroi,subnet,atlas,scm,fcm in itertools.product(subrois, subnets, atlases, sc_metrics, fc_metrics):
+        sbr = '_'.join(subroi)
+        if ( (subnet_corrs[subnet,sbr][atlas,scm,fcm,'controls'] != None) and (subnet_corrs[subnet,sbr][atlas,scm,fcm,'patients'] != None) ):
+            t,p = scipy.stats.ttest_ind(subnet_corrs[subnet,sbr][atlas,scm,fcm,'controls']['r'], subnet_corrs[subnet,sbr][atlas,scm,fcm,'patients']['r'], permutations=1000)
+            outp_scfc[subnet,sbr,atlas,scm,fcm] = {'t':t, 'p':p}
             if (p < p_min[0]):
                 p_min = [p, [subnet,atlas,scm,fcm]]
         else:
             excl_subnets.append(subnet)
 
     # plot stats
-    for subnet in subnets:
+    for subnet,subroi in itertools.product(subnets, subrois):
+        sbr = '_'.join(subroi)
         if subnet not in excl_subnets:
-            plot_scfc_pvals(outp_scfc, subnet, atlases, sc_metrics, fc_metrics)#p_min[1][2], p_min[1][3])
+            plot_scfc_pvals(outp_scfc, subnet, sbr, atlases, sc_metrics, fc_metrics)#p_min[1][2], p_min[1][3])
             for atlas in atlases:
-                plot_scfc_corr(subnet_corrs[subnet], subnet, atlas, sc_metrics, fc_metrics)
+                plot_scfc_corr(subnet_corrs[subnet,sbr], subnet, atlas, sc_metrics, fc_metrics)
