a9b984ef120b88f965a2a734e0ce7b3dbe74f853
sNaze <sebastien.naze@gmail.com>
2022-04-26 14:31:27 +1000

    updated with vPutdPFC pathway

--- voxelwise_diffusion_analysis.py
+++ voxelwise_diffusion_analysis.py
@@ -42,18 +42,18 @@ atlas_dir = '/home/sebastin/working/lab_lucac/shared/parcellations/qsirecon_atla
 atlas_cfg_path = os.path.join(atlas_dir, 'atlas_config.json')
 with open(atlas_cfg_path) as jsf:
     atlas_cfg = json.load(jsf)
-subjs = pd.read_table(os.path.join(code_dir, 'subject_list.txt'), names=['name'])['name']
+subjs = pd.read_table(os.path.join(code_dir, 'subject_list_all.txt'), names=['name'])['name']
 
 # options
 sc_metrics = ['sift'] #unecessary
-rois = ['ACC_OFC_ROI', 'PUT_PFC_ROI']
+rois = ["AccR_dPutR_vPutL_lvPFC_lPFC_dPFC_sphere6-12mm"] #['ACC_OFC_ROI', 'PUT_PFC_ROI']
 dsi_metrics = ['gfa'] #, 'iso', 'fa0', 'fa1', 'fa2']
-subroi = '3-2' #'3-1'#'3-2'  # 3-1=contralateral, 3-2=ipsilateral, 3-3=cortico-cortical
+subroi = 'vPutdPFC' #'3-2' #'3-1'#'3-2'  # 3-1=contralateral, 3-2=ipsilateral, 3-3=cortico-cortical
 #thresholds = [s+'%' for s in np.arange(0,100,20).astype(str)]
 cohorts = ['controls', 'patients']
 
 # coords for plotting global masks
-xyz = {'ACC_OFC_ROI':(20, 42, -4), 'PUT_PFC_ROI':None}
+xyz = {'ACC_OFC_ROI':(20, 42, -4), 'PUT_PFC_ROI':None, "AccR_dPutR_vPutL_lvPFC_lPFC_dPFC_sphere6-12mm":None}
 
 
 def get_tdi_img(subj, scm, roi):
@@ -88,19 +88,39 @@ def compute_tdi_maps(thresholds, args):
         for i,subj in enumerate(subjs):
             # import track density images (TDI)
             img = get_tdi_img(subj, scm, roi)
-            if img==None:
-                subjs.pop(i)
-                print("{} removed, TDI img not found".format(subj))
-                continue
+            if img==None :
+                #subjs.pop(i)
+                print("{} TDI img is not found, will apply group level TD mask instead".format(subj))
+                mask = None
+            elif np.sum(img.get_fdata())==0:
+                 print("{} TDI img is empty, will apply group level TD mask instead".format(subj))
+                 mask = None
+            else:
+                # threshold TDI to get binary mask
+                img_data = img.get_fdata().copy()
+                q_thr = np.percentile(img_data[img_data!=0], thr)
+                mask = binarize_img(img, threshold=q_thr)
 
-            # threshold TDI to get binary mask
-            img_data = img.get_fdata().copy()
-            q_thr = np.percentile(img_data[img_data!=0], thr)
-            mask = binarize_img(img, threshold=q_thr)
-            if args.plot_indiv_masks:
-                plot_roi(mask, title='_'.join([subj, roi, str(thr)+'%']),
-                         output_file=os.path.join(proj_dir, 'postprocessing', subj, '_'.join([subj, roi, str(thr)])+'.png'))
+                if args.plot_indiv_masks:
+                    plot_roi(mask, title='_'.join([subj, roi, str(thr)+'%']),
+                             output_file=os.path.join(proj_dir, 'postprocessing', subj, '_'.join([subj, roi, str(thr)])+'.png'))
 
+                if 'control' in subj:
+                    dsi_m[dsm,scm,roi,thr, 'controls']['masks'].append(mask)
+                else:
+                    dsi_m[dsm,scm,roi,thr, 'patients']['masks'].append(mask)
+            dsi_m[dsm,scm,roi,thr,subj, 'mask'] = mask
+
+    return dsi_m
+
+
+def compute_diffusion_maps(dsi_m, args):
+    """ computes the diffusion metric accross the track density mask """
+    for dsm,scm,roi,thr in itertools.product(dsi_metrics, sc_metrics, rois, args.thresholds):
+        for i,subj in enumerate(subjs):
+            # import TD map
+            mask = dsi_m[dsm,scm,roi,thr,subj, 'mask']
+            # import diffusion maps (i.e. GFA)
             img = get_diffusion_metric(subj, metric=dsm)
             if img==None:
                 subjs.pop(i)
@@ -112,17 +132,9 @@ def compute_tdi_maps(thresholds, args):
             mean_dsi = np.mean(data[data!=0])
             if 'control' in subj:
                 dsi_m[dsm,scm,roi,thr, 'controls']['mean_dsi'].append(mean_dsi)
-                if args.keep_masks:
-                    dsi_m[dsm,scm,roi,thr, 'controls']['masks'].append(mask)
-
             else:
                 dsi_m[dsm,scm,roi,thr, 'patients']['mean_dsi'].append(mean_dsi)
-                if args.keep_masks:
-                    dsi_m[dsm,scm,roi,thr, 'patients']['masks'].append(mask)
-
-    if args.save_outputs:
-        with open(os.path.join(proj_dir, 'postprocessing', 'dsi_m.pkl'), 'wb') as f:
-            pickle.dump(dsi_m, f)
+            dsi_m[dsm,scm,roi,thr,subj, 'mean_dsi'] = mean_dsi
     return dsi_m
 
 def create_df_dsi(dsi_m, thr, scm='sift', dsm='gfa'):
@@ -136,10 +148,10 @@ def create_df_dsi(dsi_m, thr, scm='sift', dsm='gfa'):
         df_dsi = df_dsi.append(df_, ignore_index=True)
     return df_dsi
 
-def create_summary():
+def create_summary(dsi_m, args):
     """ Summarize TDI outputs in dataframe """
     summary_df = pd.DataFrame()
-    for dsm,scm,roi,thr in itertools.product(dsi_metrics, sc_metrics, rois, thresholds):
+    for dsm,scm,roi,thr in itertools.product(dsi_metrics, sc_metrics, rois, args.thresholds):
         df = pd.DataFrame()
         df['subj'] = subjs.copy()
         df['mean_dsi'] = np.concatenate([dsi_m[dsm,scm,roi,thr, 'controls']['mean_dsi'], dsi_m[dsm,scm,roi,thr, 'patients']['mean_dsi']])
@@ -187,21 +199,30 @@ def compute_stats(thresholds, args):
 
 
 ## PLOTTING FUNCTIONS ##
-def plot_group_masks(dsi_m, thresholds, args):
-    for dsm,scm,roi,thr in itertools.product(dsi_metrics, sc_metrics, rois, thresholds):
+def get_group_masks(dsi_m, args):
+    for dsm,scm,roi,thr in itertools.product(dsi_metrics, sc_metrics, rois, args.thresholds):
         unscaled_img = mean_img(np.concatenate([dsi_m[dsm,scm,roi,thr,'controls']['masks'], \
                                                 dsi_m[dsm,scm,roi,thr,'patients']['masks']]))
         rescaled_img = threshold_normalize_img(unscaled_img)
-        if args.save_figs:
-            plot_roi(rescaled_img, cmap='Reds', threshold=0.00001, draw_cross=False,
-                      cut_coords=xyz[roi], alpha=0.8, title='{} avg mask at {}%'.format(roi, str(thr)),
-                      output_file=os.path.join(proj_dir, 'postprocessing', 'avg_mask_{}_{}percent.svg'.format(roi, str(thr))) )
-        if args.plot_figs:
-            plot_roi(rescaled_img, cmap='Reds', threshold=0.00001, draw_cross=False,
-                      cut_coords=xyz[roi], alpha=0.8)
+        mask = binarize_img(rescaled_img, threshold=0.5)
+
+        for i,subj in enumerate(subjs):
+            if dsi_m[dsm,scm,roi,thr,subj,'mask'] == None :
+                dsi_m[dsm,scm,roi,thr,subj,'mask'] = mask
+
+        if args.plot_group_masks:
+            if args.save_figs:
+                plot_roi(rescaled_img, cmap='Reds', threshold=0.00001, draw_cross=False,
+                          cut_coords=xyz[roi], alpha=0.8, title='{} avg mask at {}%'.format(roi, str(thr)),
+                          output_file=os.path.join(proj_dir, 'postprocessing', 'avg_mask_{}_{}percent.svg'.format(roi, str(thr))) )
+            if args.plot_figs:
+                plot_roi(rescaled_img, cmap='Reds', threshold=0.00001, draw_cross=False,
+                          cut_coords=xyz[roi], alpha=0.8)
+
+    return dsi_m
 
 
-def plot_tdi_distrib(df_dsi, args):
+def plot_tdi_distrib(df_dsi, thr, args, ylims=[0.09, 0.14]):
     fig = plt.figure(figsize=[8,4])
 
     ax1 = plt.subplot(1,3,1)
@@ -209,30 +230,30 @@ def plot_tdi_distrib(df_dsi, args):
                    inner='quartile', dodge=True, width=0.8, cut=2)
     #sbn.stripplot(data=df_dsi, y='mean_dsi', x='roi', hue='cohorts', orient='v', ax=ax1, split=True, dodge=False,
     #              size=2, edgecolor='black', linewidth=0.5, jitter=0.25)
-    plt.ylim([0.09, 0.14]);
+    plt.ylim(ylims);
     ax1.legend([])
 
     ax2 = plt.subplot(1,3,2)
-    sbn.stripplot(data=df_dsi, y='mean_dsi', x='roi', hue='cohorts', orient='v', ax=ax2, dodge=True)
-    plt.ylim([0.09, 0.14]);
+    sbn.stripplot(data=df_dsi, y='mean_dsi', x='roi', hue='cohorts', orient='v', ax=ax2, dodge=True, linewidth=1, size=2)
+    plt.ylim(ylims);
 
     ax3 = plt.subplot(1,3,3)
     bplot = sbn.boxplot(data=df_dsi, y='mean_dsi', x='roi', hue='cohorts', orient='v', ax=ax3, fliersize=0)
     #bplot.set_facecolor('blue')
     #bplot['boxes'][1].set_facecolor('orange')
     splot = sbn.swarmplot(data=df_dsi, y='mean_dsi', x='roi', hue='cohorts', orient='v', ax=ax3, dodge=True, linewidth=1, size=6, alpha=0.6)
-    plt.ylim([0.09, 0.14]);
+    plt.ylim(ylims);
     ax3.spines['top'].set_visible(False)
     ax3.spines['right'].set_visible(False)
     lgd = ax3.legend(handles=bplot.patches, labels=['HC', 'OCD'], bbox_to_anchor=(1, 1))
-    ax3.set_xticklabels(labels=['NAcc', 'dPut'], minor=True)
+    #ax3.set_xticklabels(labels=['NAcc', 'dPut'], minor=True)
     fig.tight_layout()
+
+    if args.save_figs:
+        plt.savefig(os.path.join(proj_dir, 'img', 'TD_FA_con_vs_pat_vPutdPFC_'+str(thr)+'_v02.svg'))
 
     if args.plot_figs:
         plt.show(block=False)
-
-    if args.save_figs:
-        plt.savefig(os.path.join(proj_dir, 'img', 'TD_FA_con_vs_pat_'+str(thr)+'.svg'))
 
     plt.close()
 
@@ -253,10 +274,15 @@ if __name__=='__main__':
 
     # parameters
     thresholds = np.arange(20,101,20) # in %
-    tdi_threshold_to_plot = thresholds[4] # plot 80%
+    args.thresholds = thresholds
+    tdi_threshold_to_plot = thresholds[3] # plot 80%
+
+    print(subroi)
 
     if args.compute_tdi:
         dsi_m = compute_tdi_maps(thresholds, args)
+        dsi_m = get_group_masks(dsi_m, args)
+        dsi_m = compute_diffusion_maps(dsi_m, args)
     else:
         with open(os.path.join(proj_dir, 'postprocessing', 'dsi_m.pkl'), 'rb') as f:
             dsi_m = pickle.load(f)
@@ -265,19 +291,24 @@ if __name__=='__main__':
 
     # Effect size
     df_dsi = create_df_dsi(dsi_m, tdi_threshold_to_plot)
-    cons = df_dsi[(df_dsi['cohorts']=='controls') & (df_dsi['roi']=='ACC_OFC_ROI')].mean_dsi
-    pats = df_dsi[(df_dsi['cohorts']=='patients') & (df_dsi['roi']=='ACC_OFC_ROI')].mean_dsi
-    print('Controls: mean={:.5f} std={:.5f} n={}'.format(cons.mean(), cons.std(), str(len(cons))))
-    print('Patients: mean={:.5f} std={:.5f} n={}'.format(pats.mean(), pats.std(), str(len(pats))))
-    print('cohen\'s d at threshold {} = {:.2f}'.format(str(tdi_threshold_to_plot), cohen_d(cons, pats)))
+    for roi in rois:
+      print(roi)
+      cons = df_dsi[(df_dsi['cohorts']=='controls') & (df_dsi['roi']==roi)].mean_dsi
+      pats = df_dsi[(df_dsi['cohorts']=='patients') & (df_dsi['roi']==roi)].mean_dsi
+      print('Controls: mean={:.5f} std={:.5f} n={}'.format(cons.mean(), cons.std(), str(len(cons))))
+      print('Patients: mean={:.5f} std={:.5f} n={}'.format(pats.mean(), pats.std(), str(len(pats))))
+      print('cohen\'s d at threshold {} = {:.2f}'.format(str(tdi_threshold_to_plot), cohen_d(cons, pats)))
 
-    create_summary(df_dsi, args)
+    create_summary(dsi_m, args)
 
-    if args.plot_group_masks:
-        plot_group_masks(dsi_m, thresholds)
+    #avg_mask = plot_group_masks(dsi_m, thresholds, args)
 
     if args.plot_tdi_distrib:
-        plot_tdi_distrib(df_dsi, args)
+        plot_tdi_distrib(df_dsi, tdi_threshold_to_plot, args)
+
+    if args.save_outputs:
+        with open(os.path.join(proj_dir, 'postprocessing', 'dsi_m.pkl'), 'wb') as f:
+            pickle.dump(dsi_m, f)
 
 
 
